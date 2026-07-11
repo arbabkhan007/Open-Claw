auraecosystem/Openclaw-builder

feat(subagents): add local execution backend selection

src/agents/acp-spawn.ts
@@ -73,6 +73,11 @@ import {
 } from "./acp-spawn-parent-stream.js";
 import { listAgentIds, resolveAgentConfig, resolveDefaultAgentId } from "./agent-scope.js";
 import {
+  resolveAgentExecutionPlacement,
+  type AgentExecutionPlacement,
+  type AgentExecutionPlacementRequest,
+} from "./execution-backends.js";
+import {
   findAcpUnsupportedInheritedToolAllow,
   findAcpUnsupportedInheritedToolDeny,
   formatAcpInheritedToolAllowError,
@@ -117,6 +122,7 @@ export type SpawnAcpParams = {
   thread?: boolean;
   sandbox?: SpawnAcpSandboxMode;
   streamTo?: SpawnAcpStreamTarget;
+  execution?: AgentExecutionPlacementRequest;
 };
 
 export type SpawnAcpContext = {
@@ -160,6 +166,7 @@ type SpawnAcpResultFields = {
   inlineDelivery?: boolean;
   streamLogPath?: string;
   note?: string;
+  execution?: AgentExecutionPlacement;
 };
 
 type SpawnAcpAcceptedResult = SpawnAcpResultFields & {
@@ -1218,6 +1225,18 @@ export async function spawnAcpDirect(
       error: formatAcpInheritedToolAllowError(acpUnsupportedInheritedAllow),
     });
   }
+  const executionResult = resolveAgentExecutionPlacement({
+    cfg,
+    request: params.execution,
+  });
+  if (!executionResult.ok) {
+    return createAcpSpawnFailure({
+      status: "error",
+      errorCode: "runtime_policy",
+      error: executionResult.error,
+    });
+  }
+  const execution = executionResult.execution;
 
   const spawnMode = resolveSpawnMode({
     requestedMode: params.mode,
@@ -1532,6 +1551,7 @@ export async function spawnAcpDirect(
       mode: spawnMode,
       ...(streamLogPath ? { streamLogPath } : {}),
       note: spawnMode === "session" ? ACP_SPAWN_SESSION_ACCEPTED_NOTE : ACP_SPAWN_ACCEPTED_NOTE,
+      execution,
     };
   }
 
@@ -1565,5 +1585,6 @@ export async function spawnAcpDirect(
     mode: spawnMode,
     ...(deliveryPlan.useInlineDelivery ? { inlineDelivery: true } : {}),
     note: spawnMode === "session" ? ACP_SPAWN_SESSION_ACCEPTED_NOTE : ACP_SPAWN_ACCEPTED_NOTE,
+    execution,
   };
 }

src/agents/subagent-registry.types.ts
@@ -1,4 +1,5 @@
 import type { DeliveryContext } from "../utils/delivery-context.types.js";
+import type { AgentExecutionPlacement } from "./execution-backends.js";
 import type { SubagentRunOutcome } from "./subagent-announce-output.js";
 import type { SubagentLifecycleEndedReason } from "./subagent-lifecycle-events.js";
 import type { SpawnSubagentMode } from "./subagent-spawn.types.js";
@@ -86,4 +87,5 @@ export type SubagentRunRecord = {
   attachmentsDir?: string;
   attachmentsRootDir?: string;
   retainAttachmentsOnKeep?: boolean;
+  execution?: AgentExecutionPlacement;
 };

arbabkhan007/Open-Claw

src/agents/acp-spawn.ts
@@ -82,6 +82,11 @@ import {
 } from "./acp-spawn-parent-stream.js";
 import { listAgentIds, resolveAgentConfig, resolveDefaultAgentId } from "./agent-scope.js";
 import {
+  resolveAgentExecutionPlacement,
+  type AgentExecutionPlacement,
+  type AgentExecutionPlacementRequest,
+} from "./execution-backends.js";
+import {
   findAcpUnsupportedInheritedToolAllow,
   findAcpUnsupportedInheritedToolDeny,
   formatAcpInheritedToolAllowError,
@@ -138,6 +143,7 @@ type SpawnAcpParams = {
   sandbox?: SpawnAcpSandboxMode;
   streamTo?: SpawnAcpStreamTarget;
   attachments?: AcpTurnAttachment[];
+  execution?: AgentExecutionPlacementRequest;
 };
 
 type GatewayImageAttachmentInput = {
@@ -208,6 +214,7 @@ type SpawnAcpResultFields = {
   inlineDelivery?: boolean;
   streamLogPath?: string;
   note?: string;
+  execution?: AgentExecutionPlacement;
 };
 
 type SpawnAcpAcceptedResult = SpawnAcpResultFields & {
@@ -1330,6 +1337,18 @@ export async function spawnAcpDirect(
       error: formatAcpInheritedToolAllowError(acpUnsupportedInheritedAllow),
     });
   }
+  const executionResult = resolveAgentExecutionPlacement({
+    cfg,
+    request: params.execution,
+  });
+  if (!executionResult.ok) {
+    return createAcpSpawnFailure({
+      status: "error",
+      errorCode: "runtime_policy",
+      error: executionResult.error,
+    });
+  }
+  const executionPlacement = executionResult.execution;
 
   const spawnMode = resolveSpawnMode({
     requestedMode: params.mode,
@@ -1683,6 +1702,7 @@ export async function spawnAcpDirect(
       runTimeoutSeconds,
       ...(streamLogPath ? { streamLogPath } : {}),
       note: spawnMode === "session" ? ACP_SPAWN_SESSION_ACCEPTED_NOTE : ACP_SPAWN_ACCEPTED_NOTE,
+      execution: executionPlacement,
     };
   }
 
@@ -1725,5 +1745,6 @@ export async function spawnAcpDirect(
     runTimeoutSeconds,
     ...(deliveryPlan.useInlineDelivery ? { inlineDelivery: true } : {}),
     note: spawnMode === "session" ? ACP_SPAWN_SESSION_ACCEPTED_NOTE : ACP_SPAWN_ACCEPTED_NOTE,
+    execution: executionPlacement,
   };
 }

src/agents/subagent-registry.types.ts
@@ -4,6 +4,7 @@
  * Defines execution, completion, delivery, pending-delivery, and attachment state stored for child runs.
  */
 import type { DeliveryContext } from "../utils/delivery-context.types.js";
+import type { AgentExecutionPlacement } from "./execution-backends.js";
 import type { SubagentRunOutcome } from "./subagent-announce-output.js";
 import type { SubagentLifecycleEndedReason } from "./subagent-lifecycle-events.js";
 import type { SpawnSubagentMode } from "./subagent-spawn.types.js";
@@ -146,4 +147,5 @@ export type SubagentRunRecord = {
   attachmentsDir?: string;
   attachmentsRootDir?: string;
   retainAttachmentsOnKeep?: boolean;
+  executionPlacement?: AgentExecutionPlacement;
 };